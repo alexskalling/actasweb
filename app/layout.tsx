8cca230ed71843777a799e2694fb9f71fecbef75
alexskalling <alex@skalling.com>
2025-03-25 18:22:23 -0300

    pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import { headers } from "next/headers";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -13,27 +15,77 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+let title = "Generador de Actas en minutos | ActasDeReuniones.AI";
+let description =
+  "Convierte grabaciones en pre-actas en minutos. Ahorra tiempo, cumple con la Ley 675, garantiza precisión y calidad con nuestra herramienta sencilla.";
+{
+  process.env.NEXT_PUBLIC_PAGO != "soporte" && (title = "Soporte actas");
+}
 export const metadata: Metadata = {
-  title: "Generador de Actas en minutos | ActasDeReuniones.AI",
-  description:
-    "Convierte grabaciones en pre-actas en minutos. Ahorra tiempo, cumple con la Ley 675, garantiza precisión y calidad con nuestra herramienta sencilla.",
+  title: title,
+  description: description,
 };
 
-export default function RootLayout({
+export default async function RootLayout({
+  // Añadimos 'async' aquí
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const headersList = await headers(); // Usamos 'await' aquí
+  const hostname = headersList.get("host") || "";
+  const isGeneradorDomain = hostname === "generador.actasdereuniones.ai";
+
   return (
-    <html lang="en">
+    <html lang="es">
+      <head>
+        {!isGeneradorDomain && (
+          <meta name="robots" content="noindex, nofollow" />
+        )}
+        {/* Otros elementos del head */}
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+
+        {/* Cargar el script de Meta Pixel */}
         <Script
-          src="https://checkout.wompi.co/widget.js"
-          strategy="lazyOnload"
+          strategy="afterInteractive"
+          src={`https://connect.facebook.net/en_US/fbevents.js`}
+          id="meta-pixel-script"
         />
+
+        {/* Meta Pixel Inline Script */}
+        <Script
+          id="meta-pixel-inline"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s) {
+                if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=;t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)
+              }(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '968021022066284');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
+        {/* Etiqueta <noscript> para usuarios sin JavaScript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=968021022066284&ev=PageView&noscript=1`}
+          />
+        </noscript>
+
+        {/* Google Analytics and Tag Manager */}
         <GoogleAnalytics gaId="G-VL70D0YN9S" />
         <GoogleTagManager gtmId="GTM-MRMG7JTJ" />
       </body>
