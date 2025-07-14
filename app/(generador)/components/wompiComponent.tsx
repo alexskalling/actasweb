2b77645e6b0b1acee77323217e601ef525279522
alexskalling <alex@skalling.com>
2025-07-14 16:43:59 -0500

    fix linter

--- app/(generador)/components/wompiComponent.tsx
+++ app/(generador)/components/wompiComponent.tsx
@@ -46,16 +46,6 @@ const createCheckoutInstance = (
   });
 };
 
-const formatDuration = (seconds: number): string => {
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  const secs = Math.floor(seconds % 60);
-  return [hours, minutes, secs]
-    .map((v) => (v < 10 ? "0" + v : v))
-    .filter((v, i) => v !== "00" || i > 0)
-    .join(":");
-};
-
 
 
 const tipo = process.env.NEXT_PUBLIC_PAGO;
