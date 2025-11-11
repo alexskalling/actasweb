1768ecfe1ef5c90dabbfef418b0d5b20b8299b88
akizzuki230 <guilloalva230@gmail.com>
2025-11-11 11:11:27 -0300

    fix conect db

--- lib/db/db.ts
+++ lib/db/db.ts
@@ -9,12 +9,6 @@ const client = postgres(process.env.DATABASE_URL, {
   ssl: 'require',
   connect_timeout: 30,
   max: 10,
-  idle_timeout: 20,
-  max_lifetime: 60 * 30,
-  onnotice: () => {},
-  connection: {
-    application_name: 'actasweb',
-  },
 });
 
 export const db = drizzle(client)
